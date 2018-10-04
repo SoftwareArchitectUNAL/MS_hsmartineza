28b88f92351eab3e07eb45e53f490703ead9748e
Unknown <hsmartineza@unal.edu.co>
2018-10-03 23:43:43 -0500

    Primera Version MicroService

---
+++ Controllers/EntidadesController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using EntidadesApi.Model;
+
+namespace EntidadesApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class EntidadesController : Controller
+    {
+
+
+
+        private readonly IEntidadRepository  _repository;
+        public EntidadesController(IEntidadRepository repository)
+        {
+            _repository = repository;
+
+        }
+
+
+        [HttpGet]
+        // // GET api/values/5
+        public IEnumerable<Entidad> Get()
+        {
+            return _repository.Get();
+        }
+
+        [HttpGet ("{id}")]
+        public Entidad GetEntidad(int Id){
+            
+            return _repository.GetEntidad(Id);
+        }
+
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _repository.Delete(id);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EntidadesApi.Model;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace EntidadesApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            //Configuration = configuration;
+            Configuration = configuration;
+
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<EntidadContext>(opt => opt.UseMySQL(Configuration.GetConnectionString("DbEntidades")));
+            services.AddScoped<IEntidadRepository, EntidadRepository>();
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
